aecae4e24cfd170bb9802f29c665c2616983afba
reesmcivor <reesmcivor@hotmail.com>
2023-02-03 13:34:01 +0000

    Updates

--- src/WorkingDaysService.php
+++ src/WorkingDaysService.php
@@ -4,11 +4,38 @@ namespace ReesMcIvor\WorkingDays;
 
 class WorkingDaysService {
 
+    public $countries = [];
     public $startTime = 0;
+    public $finishTime = 0;
 
-    public function setStartDate( $startTime )
+    public function setStartTime( $startTime ) : self
     {
         $this->startTime = $startTime;
+        return $this;
+    }
+
+    public function setFinishTime( $finishTime ) : self
+    {
+        $this->finishTime = $finishTime;
+        return $this;
+    }
+
+    public function setCountries( $countries = [] ) : self
+    {
+        $this->countries = $countries;
+        return $this;
+    }
+
+    public function getWorkingDays()
+    {
+        if(!$this->startTime) throw new \Exception('Start time not set');
+
+        if(!$this->finishTime) throw new \Exception('Finish time not set');
+
+        if(!$this->countries) throw new \Exception('Countries not set');
+
+        return collect($this->workingDays);
+        
     }
 
 }
