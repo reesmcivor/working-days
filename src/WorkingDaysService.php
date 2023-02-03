098e5114fa1746791e9cdf13f60a723e41ac135b
reesmcivor <reesmcivor@hotmail.com>
2023-02-03 13:44:06 +0000

    Working Dates

--- src/WorkingDaysService.php
+++ src/WorkingDaysService.php
@@ -2,6 +2,8 @@
 
 namespace ReesMcIvor\WorkingDays;
 
+use Carbon\CarbonPeriod;
+
 class WorkingDaysService {
 
     public $countries = [];
@@ -29,13 +31,24 @@ class WorkingDaysService {
     public function getWorkingDays()
     {
         if(!$this->startTime) throw new \Exception('Start time not set');
-
         if(!$this->finishTime) throw new \Exception('Finish time not set');
-
         if(!$this->countries) throw new \Exception('Countries not set');
 
+        foreach($this->getDatePeriod($this->startTime, $this->finishTime) as $date) {
+            $this->workingDays[] = $date;
+        }
         return collect($this->workingDays);
         
+    }
+
+    private function getDatePeriod( $startTime, $finishTime ) : CarbonPeriod
+    {
+        return CarbonPeriod::create($this->startDate, $this->endDate);
+    }
+
+    public function getUkHolidays()
+    {
+
     }
 
 }
